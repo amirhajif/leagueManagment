e57357ec2609baae1be09e240d680c3c77988a47
amirhajif <amirhajitabar2@gmail.com>
2022-01-02 21:52:39 +0330

    add some feature

--- leagueManagment/leagueManagment.cpp
+++ leagueManagment/leagueManagment.cpp
@@ -20,10 +20,19 @@ bool isFileEmpty(string);
 void initFiles(vector <LeagueManager>&, vector <TeamManager>&,
 	vector<Coach>&, vector<Player>&, vector<Team>&, vector<Request>&);
 
-//check username before submit not be duplicated
-bool isUserNameExist(string,vector<TeamManager>&);
+//function for  check username of teammanager before submit not be duplicated
+bool isTmUserNameExist(string,vector<TeamManager>&);
 
-//sign up team manager
+//function for  check username of coach before submit not be duplicated
+bool isCoachUserNameExist(string userName,vector<Coach>& coaches)
+{
+	for (int i = 0; i < coaches.size(); i++)
+		if (coaches[i].getUsername() == userName)
+			return true;
+	return false;
+}
+
+//function for sign up team manager
 void signUpTeamManager(vector<TeamManager>& teamManagers)
 {
 	cin.clear();
@@ -41,7 +50,7 @@ void signUpTeamManager(vector<TeamManager>& teamManagers)
 	{
 		cout << "enter user name:\t";
 		getline(cin, userName);
-		if (!isUserNameExist(userName, teamManagers))
+		if (!isTmUserNameExist(userName, teamManagers))
 			break;
 		else
 			cout << "plz select another one it`s exist!!\n";
@@ -56,19 +65,177 @@ void signUpTeamManager(vector<TeamManager>& teamManagers)
 	file.close();
 }
 
-//function for show team manager menu
-void teamManagerMenu(vector<TeamManager>& teamManagers)
+//function for add coach to coach vector
+void signUpCoach(vector<Coach>& coaches, vector<Team>& teams)
 {
-	cout << "1-logIn\n2-i`m new(sign up)\nenter option:\t";
+	//check be team available
+	if (teams.size() == 0)
+	{
+		cout << "sorry we dont have team and cant sumbit coach!!\n";
+		return;
+	}
+	cin.clear();
+	cin.ignore();
+
+	string firstName, lastName, userName, password;
+
+	cout << "enter first name:\t";
+	getline(cin, firstName);
+
+	cout << "enter last name:\t";
+	getline(cin, lastName);
+	//check user name not be duplicated
+	while (true)
+	{
+		cout << "enter user name:\t";
+		getline(cin, userName);
+		if (!isCoachUserNameExist(userName, coaches))
+			break;
+		else
+			cout << "plz select another one it`s exist!!\n";
+	}
+	cout << "plz enter password:\t";
+	getline(cin, password);
+	Team t;
+	//show list of team for submit
+
+	/*cout << "\nlist of teams available:\n";*/
+	//while (true)
+	//{
+	//	cout << "plz enter team name:\t";
+	//	int opt;
+	//	for (int i = 0; i < teams.size(); i++)
+	//	{
+	//		//should be completed........
+
+	//	}
+
+	//}
+
+	Coach coach(userName,password,firstName,lastName,t);
+	coaches.push_back(coach);
+	ofstream file(coachFile, ios::app | ios::binary);
+	file.write(reinterpret_cast<char*>(&coach), sizeof(Coach));
+	file.close();
+}
+
+//function for edit coach file base on vector
+void updateCoachFile(vector<Coach>& coaches)
+{
+	ofstream file(coachFile,ios::binary);
+	file.clear();
+	for (int i = 0; i < coaches.size(); i++)
+	{
+		Coach coach = coaches[i];
+		file.write(reinterpret_cast<char*>(&coach), sizeof(Coach));
+	}
+	file.close();
+}
+
+//function for delete coach
+void deleteCoach(vector<Coach>& coaches)
+{
+	cout << "plz enter username of coach:\t";
+	string userName;
+	getline(cin, userName);
+	fstream file(coachFile, ios::binary);
+	bool isExist = false;
+	int index = 0;
+	Coach coach;
+	while (!file.eof())
+	{
+		file.read(reinterpret_cast<char*>(&coach), sizeof(Coach));
+		if (coach.getUsername() == userName)
+		{
+			coaches.erase(coaches.begin() + index);
+			updateCoachFile(coaches);
+			isExist = true;
+			return;
+		}
+		index++;
+	}
+	if (!isExist)
+		cout << "sorry not found this Coach\n";
+	
+}
+
+//function for  login and do activity team manager
+void logInTeamManager(vector<TeamManager>& teamManagers, vector<Team>& teams, vector<Coach>& coaches)
+{
+	cin.clear();
+	cin.ignore();
+	cout << "plz enter username:\t";
+	string userName;
+	getline(cin, userName);
+	int index = -1;
+	//check username is exist and pass is correct or not
+	//if each one be invalid index equal -1
+	for (int i = 0; i < teamManagers.size(); i++)
+	{
+		if (teamManagers[i].getUsername() == userName)
+		{
+			string password;
+			cout << "plz enter password:\t";
+			getline(cin, password);
+			if (teamManagers[i].getPassword() == password)
+			{
+				index = i;
+				break;
+			}
+
+		}
+	}
+	//check index
+	if (index == -1)
+	{
+		cout << "you input invalid user/password\n";
+		return;
+	}
+	else
+	{
+		cout << "1-add coach\n2-delete coach\n3-edit coach\n4-request player\n5-delete team\n6-add player\n7-delete player\nselect option:\t";
+		int opt;
+		cin >> opt;
+		switch (opt)
+		{
+		case 1:
+			signUpCoach(coaches,teams);
+			break;
+		case 2:
+			deleteCoach(coaches);
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		default:
+			break;
+		}
+	}
+
+}
+
+//function for show team manager menu
+void teamManagerMenu(vector<TeamManager>& teamManagers, vector<Team>& teams, vector<Coach>& coaches)
+{
+	cout << "1-logIn\n2-i`m new(sign up)\n3-exit\nenter option:\t";
 	int opt;
 	cin >> opt;
 	switch (opt)
 	{
 	case 1:
-		//login and do activity
+		logInTeamManager(teamManagers,teams,coaches);
 		break;
 	case 2:
 		signUpTeamManager(teamManagers);
+		break;
+	case 3:
 		break;
 	default:
 		break;
@@ -76,9 +243,30 @@ void teamManagerMenu(vector<TeamManager>& teamManagers)
 
 }
 
+
+//function for show coach menu
+void coachMenu(vector<Team>& teams, vector<Coach>& coaches, vector<Player>& players)
+{
+	cout << "1-logIn\n2-i`m new(sign up)\n3-exit\nenter option:\t";
+	int opt;
+	cin >> opt;
+	switch (opt)
+	{
+	case 1:
+		
+		break;
+	case 2:
+		signUpCoach(coaches,teams);
+		break;
+	case 3:
+		break;
+	default:
+		break;
+	}
+}
 int main()
 {
-	TeamManager* tm = new TeamManager();
+	/*TeamManager* tm = new TeamManager();*/
 
 	//moshkel aval ke bod:
 	//2 field of request class should be pointer
@@ -88,14 +276,14 @@ int main()
 	//vaghti file binary ro misakhti va mikhasti baraye bare dovom run begiri
 	//on file khali bod va khataye memory access violation midad
 
-	Team* team = new Team("manager1", "coach1");
+	/*Team* team = new Team("manager1", "coach1");
 	Player* player = new Player
 		(
 		"username", "password", "firstname",
 		"lastname", "goalie", 120, 450, 0
 		);
 
-	Request r(team, "some thing", player, "agrumentring");
+	Request r(team, "some thing", player, "agrumentring");*/
 	//cout << r.getRequstedPlayer().getFirstName();
 
 	
@@ -115,15 +303,16 @@ int main()
 
 	while (true)
 	{
-		cout << "1-teamManager menu\n2-player menu\n3-Coach menu\n4-leagueManager\n5-exit\nenter option:\t";
+		cout << "1-teamManager menu\n2-Coach menu\n3-leagueManager\n4-exit\nenter option:\t";
 		int opt;
 		cin >> opt;
 		switch (opt)
 		{
 			case 1:
-				teamManagerMenu(teamManagers);
+				teamManagerMenu(teamManagers,teams,coaches);
 				break;
 			case 2:
+				coachMenu(teams,coaches,players);
 				break;
 			case 3:
 				break;
@@ -139,6 +328,7 @@ int main()
 	}
 	
 }
+
 void initFiles(vector <LeagueManager>& leagueManagers, 
 	vector <TeamManager>& teamManagers , vector<Coach>& coaches,
 	vector<Player>& players, vector<Team>& teams, 
@@ -286,7 +476,7 @@ void initFiles(vector <LeagueManager>& leagueManagers,
 
 }
 
-bool isUserNameExist(string userName, vector<TeamManager>& teamManagers)
+bool isTmUserNameExist(string userName, vector<TeamManager>& teamManagers)
 {
 	for (int i = 0; i < teamManagers.size(); i++)
 		if (teamManagers[i].getUsername() == userName)
